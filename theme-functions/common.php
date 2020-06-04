de3b1052be192457fdfb4651a5d2a87c77b04e4e
Denis Romanenko <povverster@gmail.com>
2020-06-04 18:25:19 +0300

    gallery function added

--- theme-functions/common.php
+++ theme-functions/common.php
@@ -6,7 +6,7 @@
  * Author: povverster (povverster@gmail.com)
  * GitHub: https://github.com/povverster
  * -----
- * Last Modified: Thursday, 4th June 2020 4:30:31 pm
+ * Last Modified: Thursday, 4th June 2020 6:25:00 pm
  * Modified By: povverster (povverster@gmail.com>)
  */
 
@@ -19,4 +19,38 @@ if (!defined('ABSPATH')) {
 function theme_template(string $category, string $name)
 {
   locate_template("theme-templates/$category/$name.php", true, false);
+}
+
+function get_page_gallery_images(string $gallery_slug): array
+{
+  $posts = get_posts([
+    'post_type' => 'cp_page_gallery',
+    'numberposts' => 1,
+    'meta_query' => [
+      'key' => 'cp_page_gallery_slug',
+      'value' => $gallery_slug
+    ]
+  ]);
+  wp_reset_postdata();
+
+  $gallery_post_id = $posts[0]->ID ?? null;
+  $gallery = get_post_gallery($gallery_post_id, false);
+  $str_attachments_ids = $gallery['ids'] ?? null;
+
+  if (empty($str_attachments_ids)) {
+    return [];
+  }
+
+  $result = [];
+
+  $attachments_ids = explode(',', $str_attachments_ids);
+  foreach ($attachments_ids as $attachment_id) {
+    $attachment = get_responsive_attachment((int) $attachment_id);
+
+    if (!empty($attachment)) {
+      $result[] = $attachment;
+    }
+  }
+
+  return $result;
 }
